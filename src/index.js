a5f47099398f8004d028ef72b2d0e213a0109ddc
xu-weixin <zealingstar@163.com>
2017-09-03 15:14:55 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+<div>
+  <h1>Hello World!</h1>
+</div>, document.getElementById('root'));
+registerServiceWorker();
